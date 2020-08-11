9a1e07b6d5950fcbd4eaa51a39fe5ff61fa34a22
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-08-11 08:24:23 +0800

    Optimize the code

--- app/src/main/java/com/winnerwinter/diceroller/MainActivity.kt
+++ app/src/main/java/com/winnerwinter/diceroller/MainActivity.kt
@@ -18,13 +18,14 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val diceImage: ImageView = findViewById(R.id.imageView)
-        when (diceRoll) {
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage.setImageResource(drawableResource)
     }
 }
